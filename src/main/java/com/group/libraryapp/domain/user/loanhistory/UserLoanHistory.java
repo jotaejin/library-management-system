2f49498cbc941c9d436de68eca467d2850c1c3e3
조태진 <ad0362320@naver.com>
2025-12-18 08:47:31 +0900

    첫 번째 커밋

---
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -0,0 +1,37 @@
+package com.group.libraryapp.domain.user.loanhistory;
+
+import com.group.libraryapp.domain.user.User;
+
+import javax.persistence.*;
+
+@Entity
+public class UserLoanHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    private User user;
+
+    private String bookName;
+
+    private boolean isReturn;
+
+    public UserLoanHistory() {
+    }
+
+    public UserLoanHistory(User user, String bookName) {
+        this.user = user;
+        this.bookName = bookName;
+        this.isReturn = false;
+    }
+
+    public void doReturn(){
+        this.isReturn = true;//반납 완료
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+}
