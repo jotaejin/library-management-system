03c95b192931d5d0e398760f56344d2fc5bc84d8
조태진 <ad0362320@naver.com>
2025-12-18 13:02:26 +0900

    fix: remove unintended exception in saveUser

--- src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
+++ src/main/java/com/group/libraryapp/service/user/UserServiceV2.java
@@ -24,7 +24,6 @@ public class UserServiceV2 {
     @Transactional
     public void saveUser(UserCreateRequest request){
         userRepository.save(new User(request.getName(),request.getAge()));
-        throw new IllegalArgumentException();
     }
 
     @Transactional(readOnly = true)
